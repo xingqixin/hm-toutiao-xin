4308be47e619804d047586e4bc08580d60bd8bf2
magicstf <752154173@qq.com>
2019-08-05 15:37:44 +0800

    设置导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ import Welcome from '@/views/welcome'
 import Artical from '@/views/artical'
 import Notfound from '@/views/404'
 
+// 拿取用户信息
+import store from '@/store'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -40,5 +43,13 @@ const router = new VueRouter({
   }
   ]
 })
-
+// 前置导航守卫
+router.beforeEach((to, from, next) => {
+  // 判断是不是是登录路由
+  if (to.path === '/login') return next()
+  // 不是登录就要判断是否已将登录
+  if (!store.getUser().token) return next('/login')
+  // 既不是登录也已经登录完毕,就是放行
+  next()
+})
 export default router
