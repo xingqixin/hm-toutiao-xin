07520d4ae070335e9e9dc090ab71d21bf5932aa0
magicstf <752154173@qq.com>
2019-08-05 15:45:32 +0800

    精简导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -44,12 +44,19 @@ const router = new VueRouter({
   ]
 })
 // 前置导航守卫
+// router.beforeEach((to, from, next) => {
+//   // 判断是不是是登录路由
+//   if (to.path === '/login') return next()
+//   // 不是登录就要判断是否已将登录
+//   if (!store.getUser().token) return next('/login')
+//   // 既不是登录也已经登录完毕,就是放行
+//   next()
+// })
+
+// 将上面的书写精简一下
 router.beforeEach((to, from, next) => {
-  // 判断是不是是登录路由
-  if (to.path === '/login') return next()
-  // 不是登录就要判断是否已将登录
-  if (!store.getUser().token) return next('/login')
-  // 既不是登录也已经登录完毕,就是放行
+  if (to.path !== '/login' && !store.getUser().token) return next('/login')
   next()
 })
+// 意思是,当你的要进入的页面不是登录页面,同时,在之前没有登陆过,即是拿取不到token信息,则就跳转去登录页面,否则的话就放行
 export default router
