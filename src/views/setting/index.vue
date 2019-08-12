d3a348ca372de59856e0c674669f0c87e43ae812
magicstf <752154173@qq.com>
2019-08-13 01:03:08 +0800

    setting基础布局实现

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,13 +1,58 @@
 <template>
   <div class="container">
-      <com-a></com-a>
-      <com-b></com-b>
+      <el-card>
+          <div slot='header'>
+            <my-bread>个人设置</my-bread>
+          </div>
+          <!-- 栅格 -->
+          <el-row>
+              <el-col :span="12">
+                  <!-- 表单 -->
+                  <el-form label-width="120px">
+                      <el-form-item label="编号:"></el-form-item>
+                      <el-form-item label="手机:"></el-form-item>
+                      <el-form-item label="媒体名称:">
+                          <el-input v-model="userForm.name"></el-input>
+                      </el-form-item>
+                      <el-form-item label="媒体介绍:">
+                          <el-input v-model="userForm.intro" type="rextarea" :rows="3"></el-input>
+                      </el-form-item>
+                      <el-form-item label="邮箱:">
+                          <el-input v-model="userForm.email"></el-input>
+                      </el-form-item>
+                      <el-form-item>
+                           <el-button type="primary">保存设置</el-button>
+                      </el-form-item>
+                  </el-form>
+              </el-col>
+              <el-col :span="12">
+                  <!-- 照片 -->
+                  <el-upload
+                    class="avatar-uploader"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :show-file-list="false">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+                 <p style="text-align:center;font-size:14px">修改头像</p>
+              </el-col>
+          </el-row>
+      </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      userForm: {
+        name: '',
+        intro: '',
+        email: ''
+      },
+      imageUrl: null
+    }
+  }
 }
 </script>
 
